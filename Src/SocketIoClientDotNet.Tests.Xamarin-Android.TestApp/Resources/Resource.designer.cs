260cd0426fd0b3b75e69e30ca1e8dbc1e79367c0
MatthiasL <matthias_ludwig@yahoo.com>
2015-08-09 14:20:55 +1000

    Xamarin-Android tests running

--- Src/SocketIoClientDotNet.Tests.Xamarin-Android.TestApp/Resources/Resource.designer.cs
+++ Src/SocketIoClientDotNet.Tests.Xamarin-Android.TestApp/Resources/Resource.designer.cs
@@ -26,8 +26,6 @@ namespace SocketIoClientDotNet.Tests.XamarinAndroid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::SocketIoClientDotNet.Tests.XamarinAndroid.Resource.String.ApplicationName;
-			global::Xamarin.Forms.Platform.Resource.String.Hello = global::SocketIoClientDotNet.Tests.XamarinAndroid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
@@ -55,25 +53,6 @@ namespace SocketIoClientDotNet.Tests.XamarinAndroid
 			}
 			
 			private Drawable()
-			{
-			}
-		}
-		
-		public partial class String
-		{
-			
-			// aapt resource value: 0x7f030001
-			public const int ApplicationName = 2130903041;
-			
-			// aapt resource value: 0x7f030000
-			public const int Hello = 2130903040;
-			
-			static String()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private String()
 			{
 			}
 		}
